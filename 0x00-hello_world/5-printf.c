63a215dbc98a420bacca05cb69e10414af3c63f4
Hamza Abdullahi <coachbabaa@gmail.com>
2022-12-08 14:34:01 +0100

    Fixed printf betty formatting style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  *
- * Return: always 0 (Success)
+ * Return: always 0
  */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
