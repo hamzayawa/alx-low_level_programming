cbd304fa7758942c4e74493ca6be75be8967d65e
Hamza Abdullahi <coachbabaa@gmail.com>
2022-12-08 14:22:39 +0100

    Updated puts scrip

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: always 0
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
