c242d3d202dcb18ae799eca76afb23e27f08dc5d
Hamza Abdullahi <coachbabaa@gmail.com>
2022-12-09 16:09:01 +0100

    Hot fix task 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
   * main - Entry point 
 	* @while - nested while loop
@@ -8,27 +7,26 @@
   */
 int main(void)
 {
-	int num1 = 0;
-	int num2;
-	while (num1 <= '9')
+	int num1 = 10;
+	while (num1 <= 19)
 	{
-		num2 = num1 + 1;
-		while (num2 <= '9')
+		int num2 = 10;
+		while (num2 <= 19)
 		{
-			if (num2 > num1)
+			if ((num2 % 10) > (num1 % 10))
 			{
-				putchar (num1);
-				putchar (num2);
-				if (num1 == '8' && num2 == '9')
+				putchar((num1 % 10) + '0');
+				putchar((num2 % 10) + '0');
+				if (num1 != 18 || num2 != 19)
 				{
-					putchar (',');
-					putchar (' ');
+					putchar(',');
+					putchar(' ');
 				}
 			}
 			num2++;
 		}
 		num1++;
 	}
-	putchar ('\n');
+	putchar('\n');
 	return (0);
 }
