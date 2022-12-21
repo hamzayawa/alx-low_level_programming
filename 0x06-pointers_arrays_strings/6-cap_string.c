8475a6d8407c6fe6f117eed6e381a25ceffeb137
Hamza Abdullahi <coachbabaa@gmail.com>
2022-12-21 21:51:53 +0100

    Capitaliza file updated

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -26,7 +26,9 @@ char *cap_string(char *str)
 				str[i - 1] == ')' ||
 				str[i - 1] == '{' ||
 				str[i - 1] == '}')
+		{
 			str[i] -= 32;
+		}
 	}
 	return (str);
 }
--- 0x06-pointers_arrays_strings/6-main.c
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
-	char *p;
+	char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+	char *ptr;
 
-	p = cap_string(s);
-	printf("%s", p);
-	printf("%s", s);
+	ptr = cap_string(str);
+	printf("%s", ptr);
+	printf("%s", str);
 	return (0);
 }
