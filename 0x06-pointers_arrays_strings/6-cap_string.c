90d428a131a65886d6c264aa5b2c9b4a1379e2c1
Hamza Abdullahi <coachbabaa@gmail.com>
2022-12-22 08:42:27 +0100

    Capitalize file updated

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,29 +6,25 @@
 */
 char *cap_string(char *str)
 {
-	int i = 0;
+	int i = 0, j;
+	char a[] = " \t\n,;.!?\"(){}";
 
-	while (str[++i])
+	while (*(str + i))
 	{
-		while (!(str[i] >= 97 && str[i] <= 122))
-			i++;
-
-		if (str[i - 1] == ' ' ||
-				str[i - 1] == '\t' ||
-				str[i - 1] == '\n' ||
-				str[i - 1] == ',' ||
-				str[i - 1] == ';' ||
-				str[i - 1] == '.' ||
-				str[i - 1] == '!' ||
-				str[i - 1] == '?' ||
-				str[i - 1] == '"' ||
-				str[i - 1] == '(' ||
-				str[i - 1] == ')' ||
-				str[i - 1] == '{' ||
-				str[i - 1] == '}')
+		if (*(str + i) >= 'a' && *(str + i) <= 'z')
 		{
-			str[i] -= 32;
+			if (i == 0)
+				*(str + i) -= 'a' - 'A';
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(str + i - 1))
+						*(str + i) -= 'a' - 'A';
+				}
+			}
 		}
+		i++;
 	}
 	return (str);
 }
