b58d50c63aa19cd5a34199011378ab25b5ed0af0
Hamza Abdullahi <coachbabaa@gmail.com>
2023-01-02 09:22:07 +0100

    Add str* files

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strchr - find char in a string
+ * @s: pointer string
+ * @c: char to search for
+ * Return: pointer to the first occurrence of the character c
+*/
+
+char *_strchr(char *s, char c)
+{
+	while (strlen(s))
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+return (0);
+}
