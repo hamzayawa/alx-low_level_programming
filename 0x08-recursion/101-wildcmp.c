80e60c77bb0f0c0b6ec5f25c8d5b0f733a31412d
Hamza Abdullahi <coachbabaa@gmail.com>
2023-01-04 21:48:40 +0100

    Add Task101 files

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares two strings and returns 1
+ * @s1: string compared
+ * @s2: string compared against
+ *
+ * Return: 1 if considered identical, 0 otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+			return (1);
+	}
+	return (0);
+}
